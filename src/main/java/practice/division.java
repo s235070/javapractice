c2bf1229149da5880c09bf120ef2f8dfa2b0db1f
Hadziosmanovicb <144700262+Hadziosmanovicb@users.noreply.github.com>
2024-07-09 23:05:06 +0200

    divison changed

--- src/main/java/practice/division.java
+++ src/main/java/practice/division.java
@@ -3,6 +3,6 @@ package practice;
 public class division {
     public static double division(double a, double b) {
         return a / b;
-        System.out.println("You can divide by 0, it will cause error");
+
     }
 }
--- src/main/java/practice/main.java
+++ src/main/java/practice/main.java
@@ -3,6 +3,6 @@ package practice;
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Worlddddddd!");
+        System.out.println("Choose an operator: +,-,* or /");
     }
 }
